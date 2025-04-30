a9dc415b2b7a8aaa70b9c741695f1b0a0312fc95
makhfiratradjabova <makhfiratradjabova@gmail.com>
2025-04-30 10:38:26 +0500

    Create thirdlesson.sql

---
+++ lesson-3/homework/thirdlesson.sql
@@ -0,0 +1,11 @@
+Data:
+Data refers to raw facts, figures, or information that can be recorded and analyzed. There are different forms of data such as text, numbers, images, or audio. It becomes meaningful only when processed or organized.
+
+Database:
+A database is an organized collection of data stored electronically, designed to efficiently store, manage, and retrieve information. Databases can handle large amounts of data and support operations such as searching, updating, and reporting.
+
+Relational Database:
+A relational database is a type of database that stores data in structured formats, using tables with rows and columns. Each table (also called a relation) represents a specific entity, and relationships between tables are established using keys (like primary and foreign keys). It is managed using relational database management systems (RDBMS) like MySQL, PostgreSQL, or Oracle.
+
+Table:
+A table is a collection of related data entries in a relational database, organized in rows and columns.
